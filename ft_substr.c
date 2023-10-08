b181515d57d115a850d59bf8a6ae8658bd5892ed
kfandrew910 <kfandrew910@gmail.com>
2023-10-08 04:00:24 +0200

    substr memory errors corrected

--- ft_substr.c
+++ ft_substr.c
@@ -6,14 +6,16 @@
 /*   By: akovacs- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 23:54:46 by akovacs-          #+#    #+#             */
-/*   Updated: 2023/10/08 00:15:53 by akovacs-         ###   ########.fr       */
+/*   Updated: 2023/10/08 03:59:18 by akovacs-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <unistd.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
+	char	*final;
 	int		i;
 
 	i = 0;
@@ -25,11 +27,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (start > (unsigned int)ft_strlen((char *)s))
 		return (ft_strdup(""));
-	while (len--)
+	while (len-- && s[i])
 	{
 		ptr[i] = s[start + i];
 		i++;
 	}
 	ptr[i] = 0;
-	return (ptr);
+	final = ft_strdup(ptr);
+	return (final);
 }
